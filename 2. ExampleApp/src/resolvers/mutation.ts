a0857b7dd2d8674b70c6ac69e00df5f4d97647b3
Oscar <oscarrc.web@gmail.com>
2021-03-10 17:03:03 +0100

    [ADD] Mutations

---
+++ 2. ExampleApp/src/resolvers/mutation.ts
@@ -0,0 +1,8 @@
+import { IResolvers } from 'graphql-tools';
+import { database } from '../data/data.store';
+
+//Resolver tipos incrustados
+const mutation: IResolvers = {
+};
+
+export default mutation;
--- 2. ExampleApp/src/resolvers/resolvers.map.ts
+++ 2. ExampleApp/src/resolvers/resolvers.map.ts
@@ -1,10 +1,12 @@
 import { IResolvers } from 'graphql-tools';
+import mutation from './mutation';
 import query from './query';
 import type from './type';
 
 const resolvers: IResolvers = {
     ...query,
-    ...type
+    ...type,
+    ...mutation
 }
 
 export default resolvers;
