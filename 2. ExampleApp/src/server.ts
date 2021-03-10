bf023f26bf5cde67cd0e346f94e52f662b3cbe04
Oscar <oscarrc.web@gmail.com>
2021-03-10 10:29:33 +0100

    [MOD] Configure apollo server

--- 2. ExampleApp/src/server.ts
+++ 2. ExampleApp/src/server.ts
@@ -2,18 +2,28 @@ import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
 import { createServer } from 'http';
+import { ApolloServer } from 'apollo-server-express';
+
+import expressPlayground from 'graphql-playground-middleware-express';
+import schema from './schema';
 
 const PORT = 5200;
 const app = express();
 const httpServer = createServer(app);
+const server = new ApolloServer({
+    schema,
+    introspection: true
+});
 
 app.use(cors())
    .use(compression());
 
-app.get('/', (req, res) => {
-    res.send("Bienvenido a la academia");
-})
+server.applyMiddleware({ app });
+
+app.get('/', expressPlayground({
+    endpoint: "/graphql"
+}))
 
 httpServer.listen(PORT, () => {
-    console.log(`Academia está online en http://localhost:${PORT}`)
-})
+    console.log(`Academia está online en http://localhost:${PORT}/graphql`)
+});
