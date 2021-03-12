ca31f18e47d2b0b146799265c05901ac671e3fdb
Oscar <oscarrc.web@gmail.com>
2021-03-12 12:53:00 +0100

    [MOD] Check round lower limit

--- 3. RESTtoGraphQL/src/lib/utils.ts
+++ 3. RESTtoGraphQL/src/lib/utils.ts
@@ -4,15 +4,12 @@ const getWikipediaMobileUrl = (url: String) => {
 
 const checkYear = (year: String) => {    
     const currentYear = new Date().getFullYear();
-    if(isNaN(+year) || +year < 1950 || +year > currentYear){
-        return String(currentYear);
-    }
-
+    if(isNaN(+year) || +year < 1950 || +year > currentYear) return String(currentYear);
     return year;
 }
 
 const checkRound = (round: Number) => {    
-    if(round >= 100) return 1;
+    if(round >= 100 || round < 1) return 1;
     return round;
 }
 
