fc577b971a764f6ffe69ff5fd25ecfa76a64779a
Oscar <oscarrc.web@gmail.com>
2021-03-12 10:30:23 +0100

    [ADD] Query seasonsList

--- 3. RESTtoGraphQL/src/data/data-seasons.ts
+++ 3. RESTtoGraphQL/src/data/data-seasons.ts
@@ -5,4 +5,12 @@ export class SeasonsData extends F1 {
     constructor(){
         super();
     }
+
+    async getSeasons(){
+        return await this.get("seasons.json?limit=80", {
+            cacheOptions: {
+                ttl: 60
+            }
+        })
+    }
 }
--- 3. RESTtoGraphQL/src/resolvers/query.ts
+++ 3. RESTtoGraphQL/src/resolvers/query.ts
@@ -2,6 +2,9 @@ import { IResolvers } from 'graphql-tools';
 
 const query: IResolvers = {
     Query: {
+        async seasonsList(_: void, __: void, { dataSources }){
+            return await dataSources.seasons.getSeasons().then( (data:any) =>  data.MRData.SeasonTable.Seasons );
+        }
     }
 };
 
