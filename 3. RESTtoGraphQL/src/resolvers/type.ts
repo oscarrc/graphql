318b2dfdbd48b672ddb404e91ce790a8b0ded0a5
Oscar <oscarrc.web@gmail.com>
2021-03-12 11:45:02 +0100

    [MOD] Types for Race, Circuit and Location renamed properties

--- 3. RESTtoGraphQL/src/resolvers/type.ts
+++ 3. RESTtoGraphQL/src/resolvers/type.ts
@@ -5,6 +5,18 @@ const type: IResolvers = {
     Season: {
         year: parent => parent.season,
         urlMobile: parent => getWikipediaMobileUrl(parent.url)
+    },
+    Race: {
+        name: parent => parent.raceName,
+        circuit: parent => parent.Circuit
+    },
+    Circuit: {
+        id: parent => parent.circuitId,
+        name: parent => parent.circuitName,
+        location: parent => parent.Location
+    },
+    Location: {
+        lon: parent => parent.long
     }
 };
 
