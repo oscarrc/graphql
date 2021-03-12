0b15b9fa068c9c8477332d374a8e77fc37a11233
Oscar <oscarrc.web@gmail.com>
2021-03-12 11:43:30 +0100

    [ADD] Get races by year query

--- 3. RESTtoGraphQL/src/resolvers/query.ts
+++ 3. RESTtoGraphQL/src/resolvers/query.ts
@@ -4,6 +4,9 @@ const query: IResolvers = {
     Query: {
         async seasonsList(_: void, __: void, { dataSources }){
             return await dataSources.seasons.getSeasons().then( (data:any) =>  data.MRData.SeasonTable.Seasons );
+        },
+        async racesByYear(_: void, { year }, { dataSources }){
+            return await dataSources.races.getYear( year ).then( (data:any) => data.MRData.RaceTable.Races);
         }
     }
 };
