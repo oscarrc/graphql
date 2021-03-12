d65bab41c35f23befb3ab7e7f7a6afddcc5dbf7b
Oscar <oscarrc.web@gmail.com>
2021-03-12 10:22:44 +0100

    [ADD] Seson schema definition

--- 3. RESTtoGraphQL/src/resolvers/query.ts
+++ 3. RESTtoGraphQL/src/resolvers/query.ts
@@ -2,15 +2,6 @@ import { IResolvers } from 'graphql-tools';
 
 const query: IResolvers = {
     Query: {
-        hello(): string {
-            return 'Hello world!!';
-        },
-        helloWithName(_: void, args): string {
-            return `Hello ${args.name}!!`;
-        },
-        helloToGraphQLCourse(): string {
-            return 'Hello to GraphQL Course!!';
-        }
     }
 };
 
